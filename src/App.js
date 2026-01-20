06dd2d9e25769bb2df8ad15d12bb4f8f6c0bd66e
unknown <jcwrubu@gmail.com>
2026-01-20 22:39:24 +0900

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import Button from "./button";
 import { useState, useEffect } from "react";
 
-
 function App() {
-  const [counter, setValue] = useState(0);
-  const onClick = () => setValue((prev) => prev + 1);
-  console.log("I run all the time");
-  const iRunOnlyOnce = () => console.log("I run only once");
-  useEffect(iRunOnlyOnce, []);
+
   return (
     <div>
-    <h1>{counter}</h1>
-    <button onClick = {onClick}>Click me</button>
     </div>
-    
+
   );
 }
 
